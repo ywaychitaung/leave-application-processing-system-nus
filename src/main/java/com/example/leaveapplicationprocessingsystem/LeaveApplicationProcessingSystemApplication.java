5f616fc3e5fb78eba7728adc23406a61adfc6780
ywaychitaung <ywaychitaung@gmail.com>
2023-12-14 14:18:34 +0800

    Init

---
+++ src/main/java/com/example/leaveapplicationprocessingsystem/LeaveApplicationProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaveapplicationprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApplicationProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveApplicationProcessingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leaveapplicationprocessingsystem/LeaveApplicationProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaveapplicationprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveApplicationProcessingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
