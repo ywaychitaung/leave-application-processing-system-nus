831e9eddf8abfe68b875c47f330d6c0887b9b9c7
Huruoqing <huruoqing@qq.com>
2023-12-18 18:59:10 +0800

    LeaveEntitlement table added into mysql.

--- src/main/java/com/example/leaveapplicationprocessingsystem/entity/LeaveEntitlement.java
+++ src/main/java/com/example/leaveapplicationprocessingsystem/entity/LeaveEntitlement.java
@@ -1,11 +1,38 @@
 package com.example.leaveapplicationprocessingsystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "leave_entitlement")
 public class LeaveEntitlement {
+    @Id
+    private Integer userId;  // Assuming this is a foreign key referencing the User table
 
+    @Column(name = "annual_leave_remaining")
+    private Integer annualLeaveRemaining;
+
+    @Column(name = "medical_leave_remaining")
+    private Integer medicalLeaveRemaining;
+
+    @Column(name = "compensation_leave_remaining")
+    private Integer compensationLeaveRemaining;
+
+    // Default constructor
+    public LeaveEntitlement() {
+    }
+
+    // Constructor with fields
+    public LeaveEntitlement(Integer userId, Integer annualLeaveRemaining, Integer medicalLeaveRemaining, Integer compensationLeaveRemaining) {
+        this.userId = userId;
+        this.annualLeaveRemaining = annualLeaveRemaining;
+        this.medicalLeaveRemaining = medicalLeaveRemaining;
+        this.compensationLeaveRemaining = compensationLeaveRemaining;
+    }
+
+    // Getters and Setters
+    // ...
+
+    // Other methods if needed
+    // ...
 }
+
